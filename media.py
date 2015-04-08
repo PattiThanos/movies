733a9c768121c617623c543d377bf8b53345fb5b
PattiThanos <pt8972@att.com>
2015-04-08 09:07:05 -0500

    Update media.py

--- media.py
+++ media.py
@@ -5,12 +5,17 @@ class Movie():
     
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     
+    #init is a function or constructor. Self refers to movie object
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        #initialize movie variables
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+    #define function to open browser and play video
+    #this is an instance method because it is defined inside a class
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
+
 
 
